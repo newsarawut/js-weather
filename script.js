260de3e3e7bf70c927a712f4581d31137e639c19
New Sarawut <126350332+newsarawut@users.noreply.github.com>
2023-07-05 16:27:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const appKey = '98695508075280f6c728403f118ca17a'
+
+const searchButton = document.querySelector('#search-btn')
+const searchInput = document.querySelector('#search-txt')
+const cityName = document.querySelector('.city')
+const icon = document.querySelector('.weather-icon')
+const temperature = document.querySelector('.temp')
+const humidity = document.querySelector('.humidity')
+const wind = document.querySelector('.wind')
+
+searchButton.addEventListener("click", findWeatherDetails);
+searchInput.addEventListener("keyup", enterPressed);
+
+function enterPressed(event) {
+    if (event.key == "Enter") {
+        findWeatherDetails();
+    }
+}
+
+function findWeatherDetails() {
+    if (searchInput.value === "") {
+    
+    } else {
+        const searchLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid=" + appKey;
+        httpRequestAsync(searchLink, theResponse);
+        document.querySelector(".error").style.display = "none"
+        document.querySelector(".weather").style.display = "block"
+    }
+}
+
+function theResponse(response) {
+    const jsonObject = JSON.parse(response);
+    cityName.innerHTML = jsonObject.name;
+    icon.src = 'http://openweathermap.org/img/w/' + jsonObject.weather[0].icon + ".png";
+    temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°C";
+    humidity.innerHTML = jsonObject.main.humidity + "%";
+    wind.innerHTML = jsonObject.wind.speed + " km/hr"
+
+}
+
+function httpRequestAsync(url, callback) {
+    const httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = () => {
+        if(httpRequest.readyState == 4 && httpRequest.status == 200) {
+            callback(httpRequest.responseText);
+        }
+    }
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
+}
